545efd73af47adb07b232e26b1c6cfd71afc3860
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2023-12-08 17:17:10 -0500

    More troubleshooting
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- lib/git.bash
+++ lib/git.bash
@@ -5,6 +5,13 @@ set -euo pipefail
 function repo_from_origin() {
   local origin
   local without_prefix
+  local dir
+
+  dir=$(plugin_read_config DIR '')
+
+  if [ ! -Z $dir ]; then
+    cd $dir
+  fi 
 
   origin="$(git remote get-url origin)"
   echo $origin
